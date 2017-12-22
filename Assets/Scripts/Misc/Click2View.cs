c2a860845f9ec88a64bcb12c708388fab1b454a5
galhajaj <galhajaj@gmail.com>
2017-12-22 15:38:05 +0200

    before view manager

---
+++ Assets/Scripts/Misc/Click2View.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click2View : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnMouseDown()
+    {
+        // view manager load view...
+    }
+}
