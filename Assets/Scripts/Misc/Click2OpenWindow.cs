5bc7f911ae8cb0cd93fb5eefa61cf0bef076dffb
galhajaj <galhajaj@gmail.com>
2017-12-24 22:29:58 +0200

    more

---
+++ Assets/Scripts/Misc/Click2OpenWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click2OpenWindow : MonoBehaviour
+{
+    [SerializeField]
+    private string _windowName = "";
+
+    void OnMouseDown()
+    {
+        SM.WindowManager.LoadWindow(_windowName);
+    }
+}
