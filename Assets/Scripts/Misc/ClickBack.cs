95603f78c65aee0dc6905a6666a5ffddc73eca6e
galhajaj <galhajaj@gmail.com>
2017-12-31 23:40:14 +0200

    fix back from inventory - to village or dungeon

---
+++ Assets/Scripts/Misc/ClickBack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBack : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        if (Party.Instance.Loaction == "Village")
+            WindowManager.Instance.LoadWindow("Village");
+        else
+            WindowManager.Instance.LoadWindow("Dungeon");
+    }
+}
