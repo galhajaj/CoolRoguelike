fbd317a9e61610831c3929572d27df0152df4146
galhajaj <galhajaj@gmail.com>
2018-01-02 18:19:39 +0200

    update colors of portrait units

--- Assets/Scripts/General/Portrait.cs
+++ Assets/Scripts/General/Portrait.cs
@@ -24,5 +24,21 @@ public class Portrait : MonoBehaviour
         // TODO: improve performance
         _lifeUnitsGrid.Rebuild(1, _creature.MaxHearts);
         _spellUnitsGrid.Rebuild(1, _creature.MaxMana);
+
+        updateUnitsColor(_lifeUnitsGrid, _creature.Hearts, _creature.MaxHearts);
+        updateUnitsColor(_spellUnitsGrid, _creature.Mana, _creature.MaxMana);
+        updateUnitsColor(_actionUnitsGrid, _creature.ActionUnits, ConstsManager.Instance.MAX_ACTION_UNITS);
+    }
+
+    private void updateUnitsColor(Grid grid, int number, int maxNumber)
+    {
+        List<GridElement> elements = grid.Elements;
+        for (int i = 0; i < elements.Count; ++i)
+        {
+            if (i >= number)
+            {
+                elements[i].GetComponent<SpriteRenderer>().color = Color.black;
+            }
+        }
     }
 }
