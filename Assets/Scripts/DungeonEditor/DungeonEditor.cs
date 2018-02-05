cb0334be7b59f47b3fc15725fcfba3e0170ef514
galhajaj <galhajaj@gmail.com>
2018-02-05 23:25:11 +0200

    dungeon editor - great advancement

---
+++ Assets/Scripts/DungeonEditor/DungeonEditor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonEditor : Singleton<DungeonEditor>
+{
+    private const string CURRENT_PLAYER = "Player1"; // temp...hard coded in the meantime. get it from text somehow
+    private const string SAVE_FILE_PATH = @"c:/temp"; // change location and name to be for the current player
+
+    [SerializeField]
+    private Grid _grid = null;
+    [SerializeField]
+    private InputField _inputField = null;
+
+    private DungeonSaveData _dungeonSaveData = new DungeonSaveData();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        checkLeftClickOnTile();
+        checkRightClickOnTile();
+    }
+
+    public void ClickSave()
+    {
+        /*string path = Application.dataPath + "/dungeons";
+
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);*/
+
+        Utils.WriteToBinaryFile(SAVE_FILE_PATH + "/" + _inputField.text + ".dat", _dungeonSaveData);
+    }
+
+    public void ClickLoad()
+    {
+        clear();
+    }
+
+    private void checkRightClickOnTile()
+    {
+        if (!Input.GetMouseButton(1))
+            return;
+
+        LayerMask layerMask = (1 << LayerMask.NameToLayer("DungeonTile"));
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0.0F, layerMask);
+        if (hit.collider == null)
+            return;
+
+        DungeonTile targetTile = hit.collider.GetComponent<DungeonTile>();
+        if (targetTile == null)
+            return;
+
+        targetTile.Clear();
+    }
+
+    private void checkLeftClickOnTile()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+
+        LayerMask layerMask = (1 << LayerMask.NameToLayer("DungeonTile"));
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0.0F, layerMask);
+        if (hit.collider == null)
+            return;
+
+        DungeonTile targetTile = hit.collider.GetComponent<DungeonTile>();
+        if (targetTile == null)
+            return;
+
+        if (targetTile.transform.childCount > 0)
+            return;
+
+        if (ObjectSelector.Instance.SelectedObjectName == "")
+            return;
+
+        string objectPath = ObjectSelector.Instance.ObjectsFolderName + "/" + ObjectSelector.Instance.SelectedObjectName;
+        GameObject prefabToCreate = Resources.Load<GameObject>(objectPath);
+        if (prefabToCreate == null)
+        {
+            Debug.LogError(objectPath + " resource not exists");
+            return;
+        }
+
+        GameObject instance = Instantiate(prefabToCreate);
+        instance.transform.position = targetTile.transform.position;
+        instance.transform.parent = targetTile.transform;
+    }
+
+    private void clear()
+    {
+        foreach (DungeonTile tile in _grid.Elements)
+            tile.Clear();
+    }
+}
---
+++ Assets/Scripts/DungeonEditor/ObjectSelector.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSelector : Singleton<ObjectSelector>
+{
+    public string ObjectsFolderName = "Stuff";
+    public string SelectedObjectName { get; private set; }
+
+    [SerializeField]
+    private Grid _grid = null;
+
+    [SerializeField]
+    private Text _textSelected = null;
+
+
+    void Start()
+    {
+        StartCoroutine(fillGridCoroutine());
+    }
+
+    IEnumerator fillGridCoroutine()
+    {
+        yield return new WaitForSeconds(0.25F);
+        fillGridWithSelectedObjects();
+    }
+
+    void Update()
+    {
+        checkLeftClickOnTile();
+    }
+
+    // toggle between stuff/portals/items/creatures
+    public void ToggleObjectType()
+    {
+        if (ObjectsFolderName == "Stuff")
+            ObjectsFolderName = "Portals";
+        else if (ObjectsFolderName == "Portals")
+            ObjectsFolderName = "Items";
+        else if (ObjectsFolderName == "Items")
+            ObjectsFolderName = "Creatures";
+        else if (ObjectsFolderName == "Creatures")
+            ObjectsFolderName = "Stuff";
+
+        Debug.Log("Loading " + ObjectsFolderName);
+
+        fillGridWithSelectedObjects();
+    }
+
+    private void fillGridWithSelectedObjects()
+    {
+        // clear all
+        foreach (GridElement tile in _grid.Elements)
+            if (tile.transform.childCount > 0)
+                Destroy(tile.transform.GetChild(0).gameObject);
+
+        // get all object from resources folder
+        GameObject[] allObjects = Resources.LoadAll<GameObject>(ObjectsFolderName);
+
+        // create instance of each object and place it inside the grid
+        for (int i = 0; i < allObjects.Length; ++i)
+        {
+            GameObject objInstance = Instantiate(allObjects[i]);
+            objInstance.transform.position = _grid.Elements[i].transform.position;
+            objInstance.transform.parent = _grid.Elements[i].transform;
+        }
+    }
+
+    private void checkLeftClickOnTile()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        LayerMask layerMask = (1 << LayerMask.NameToLayer("SelectorTile"));
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0.0F, layerMask);
+        if (hit.collider == null)
+            return;
+
+        SelectedObjectName = hit.collider.transform.GetChild(0).name.Split('(')[0].Trim();
+        _textSelected.text = SelectedObjectName;
+    }
+}
