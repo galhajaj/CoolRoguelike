863a92ea1147123f0486c11734ba24bacda83485
galhajaj <galhajaj@gmail.com>
2018-02-09 16:57:42 +0200

    mouse hoverer & arrange items in resources

---
+++ Assets/Scripts/DungeonEditor/MouseHovering.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHovering : MonoBehaviour
+{
+    [SerializeField]
+    private Text _textHovering = null;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        updateCheckHover();
+    }
+
+    private void updateCheckHover()
+    {
+        LayerMask layerMask = (1 << LayerMask.NameToLayer("DungeonTile"));
+        layerMask |= (1 << LayerMask.NameToLayer("SelectorTile"));
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0.0F, layerMask);
+        if (hit.collider == null)
+        {
+            _textHovering.text = "";
+            return;
+        }
+
+        _textHovering.text = "";
+        foreach (Transform transform in hit.collider.transform)
+        {
+            _textHovering.text += transform.name;
+        }
+    }
+}
