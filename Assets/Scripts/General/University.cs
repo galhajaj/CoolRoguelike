ead2d1b0ffad32d220356e24c49331851d483f3e
galhajaj <galhajaj@gmail.com>
2018-03-18 15:08:35 +0200

    starting out the university skills system

--- Assets/Scripts/General/University.cs
+++ Assets/Scripts/General/University.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class University : MonoBehaviour
 {
+    [SerializeField]
+    private GenericGrid _grid = null;
+
+    public List<string> Skills = new List<string>();
 
 	void Start ()
     {
-		
-	}
+        WindowManager.Instance.Event_WindowLoaded += universityWindowLoaded;
+    }
 	
 	void Update ()
     {
-		
+        
 	}
+
+    private void universityWindowLoaded()
+    {
+        if (!WindowManager.Instance.IsCurrentWindow(Consts.WindowNames.UNIVERSITY))
+            return;
+
+        _grid.Rebuild(Skills.Count);
+
+        for (int i = 0; i < Skills.Count; ++i)
+        {
+            Sprite skillSprite = Resources.Load<Sprite>("Skills/" + Skills[i]);
+            _grid.GetElement(i).transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = skillSprite;
+        }
+    }
 }
