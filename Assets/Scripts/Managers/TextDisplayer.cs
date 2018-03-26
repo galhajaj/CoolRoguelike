52e4c4534a2251ed8213948304be0e67a4889e5d
galhajaj <galhajaj@gmail.com>
2018-03-26 09:50:47 +0300

    show in side bar details of creatures and items

--- Assets/Scripts/Managers/TextDisplayer.cs
+++ Assets/Scripts/Managers/TextDisplayer.cs
@@ -13,34 +13,91 @@ public class TextDisplayer : Singleton<TextDisplayer>
 
     [SerializeField]
     private int _sideBarWordWrapWidth = 25;
-
+    // ====================================================================================================== //
     void Start ()
     {
         //SetSideBarText("sdfsdf sdfsfdsfsdf <color=green>green</color> sdf <color=blue>sdfsdf</color> dsf sdf dsf df dsf sdfsdf dsfdfd fd df dfdfsdfsfdf dfdfdfdfdf dfdfdf dfd df df dfdfd df dfdfdf dfdfdfd dfdf df d");
     }
-	
-	void Update ()
+    // ====================================================================================================== //
+    void Update ()
     {
         
     }
-
+    // ====================================================================================================== //
+    void LateUpdate()
+    {
+        _sideBarText.text = "";
+        showCreatureDetails();
+        showPartyMemberDetails();
+        showItemDetails();
+    }
+    // ====================================================================================================== //
     public void SetMainCaption(string text)
     {
         _mainCaption.text = text;
     }
-
+    // ====================================================================================================== //
     public void SetDescriptionLine(string text)
     {
         _descriptionLine.text = text;
     }
-
+    // ====================================================================================================== //
     public void SetSideBarText(string text)
     {
-        text = WrapText(text, _sideBarWordWrapWidth);
+        //text = WrapText(text, _sideBarWordWrapWidth);
         _sideBarText.text = text;
         //FitToWidth(_sideBarWidth, _sideBarText);
     }
+    // ====================================================================================================== //
+    private void showCreatureDetails()
+    {
+        DungeonTile dungeonTile = Utils.GetObjectUnderCursor<DungeonTile>("DungeonTile");
+        if (dungeonTile == null)
+            return;
 
+        Creature creature = dungeonTile.GetContainedCreature();
+        if (creature == null)
+            return;
+
+        _sideBarText.text = "<color=red>" + Utils.GetCleanName(creature.name) + "</color>\n";
+
+        foreach (var obj in creature.Stats)
+        {
+            _sideBarText.text += obj.Key.ToString() + ": " + obj.Value.ToString() + "\n";
+        }
+    }
+    // ====================================================================================================== //
+    private void showPartyMemberDetails()
+    {
+        Portrait portrait = Utils.GetObjectUnderCursor<Portrait>("Portrait");
+        if (portrait == null)
+            return;
+
+        _sideBarText.text = "<color=green>" + Utils.GetCleanName(portrait.Creature.name) +  "</color>\n";
+
+        foreach (var obj in portrait.Creature.Stats)
+        {
+            _sideBarText.text += obj.Key.ToString() + ": " + obj.Value.ToString() + "\n";
+        }
+    }
+    // ====================================================================================================== //
+    private void showItemDetails()
+    {
+        Item item = Utils.GetObjectUnderCursor<Item>("Item");
+        if (item == null)
+            return;
+
+        if (item.State == ItemState.GROUND)
+            return;
+
+        _sideBarText.text = "<color=blue>" + Utils.GetCleanName(item.name) + "</color>\n";
+
+        foreach (var obj in item.Stats)
+        {
+            _sideBarText.text += obj.Key.ToString() + ": " + obj.Value.ToString() + "\n";
+        }
+    }
+    // ====================================================================================================== //
     // ########################################################################################
     // helper functions for text mesg word wrap
     // ########################################################################################
