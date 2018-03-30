c8cdfa0681a9e6bbdf1f98a9f13740164473f757
galhajaj <galhajaj@gmail.com>
2018-03-30 12:19:48 +0300

    animation for portraits!!!

--- Assets/Scripts/General/Portrait.cs
+++ Assets/Scripts/General/Portrait.cs
@@ -17,13 +17,36 @@ public class Portrait : MonoBehaviour
     [SerializeField]
     private SpriteRenderer _frameRenderer = null;
 
+    private float _changeAnimationMinTime = 3.0F;
+    private float _changeAnimationMaxTime = 10.0F;
+    private float _timeToChangeAnimation;
+
     void Start ()
     {
-
+        _timeToChangeAnimation = Random.Range(_changeAnimationMinTime, _changeAnimationMaxTime);
     }
 
     void Update()
     {
+        // look right/left animation
+        if (_timeToChangeAnimation > 0)
+        {
+            _timeToChangeAnimation -= Time.deltaTime;
+        }
+        else
+        {
+            _timeToChangeAnimation = Random.Range(_changeAnimationMinTime, _changeAnimationMaxTime);
+            int rand = Random.Range(0, 5);
+            string triggerName;
+            if (rand == 0)
+                triggerName = "LookRightTrigger";
+            else if (rand == 1)
+                triggerName = "LookLeftTrigger";
+            else
+                triggerName = "BlinkTrigger";
+            GetComponent<Animator>().SetTrigger(triggerName);
+        }
+
         // frame visibility
         bool isSelectedMember = (Party.Instance.SelectedMember == _creature);
         _frameRenderer.enabled = isSelectedMember;
