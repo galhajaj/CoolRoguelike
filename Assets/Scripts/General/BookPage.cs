72ac80b25c9d033d47bf88bdb1e70685d75db3eb
galhajaj <galhajaj@users.noreply.github.com>
2018-04-05 16:44:41 +0300

    vacation commit - upgrade to version 2017.4 & graphics improvement

--- Assets/Scripts/General/BookPage.cs
+++ Assets/Scripts/General/BookPage.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class BookPage : GameButton
 {
-    public int PageNumber;
+    [SerializeField]
+    private TextMesh _textPageNumber = null;
+
+    private int _pageNumber = 0;
+    public int PageNumber
+    {
+        get { return _pageNumber; }
+        set
+        {
+            _pageNumber = value;
+            _textPageNumber.text = _pageNumber.ToString();
+        }
+    }
 
     protected override void afterClicked()
     {
